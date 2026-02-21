6ed11344c1a32d8704e876adfee80fcfd24649f2
Balint000 <erdeibalint01@gmail.com>
2026-02-21 11:09:31 +0100

    hibak javitva

--- main.cpp
+++ main.cpp
@@ -4,29 +4,29 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // Hiba_1: NELEMENTS nincs definialva (N_ELEMENTS helyes)
-    int *b = new int[NELEMENTS]; 
-    // Hiba_2: szimpla idezojel; hianyzo pontosvesszo
-    std::cout << '1-100 ertekek duplazasa' 
-    // Hiba_3: hianyos for ciklus
-    for (int i = 0;) 
+    int *b = new int[N_ELEMENTS]; 
+    std::cout << "1-100 ertekek duplazasa" << std::endl; 
+
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
         b[i] = i * 2;
     }
-    // Hiba_4: hibas feltetel: a feltetel (i) miatt 0-nal azonnal kilep
-    for (int i = 0; i; i++) 
+
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        std::cout << "Ertek:" // Hiba_5: hianyzo pontosvesszo
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
+
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // Hiba_6: nincs inicializalva (memoria-szemet lesz benne)
-    // Hiba_7: vesszo hasznalata pontosvesszo helyett
-    for (int i = 0; i < N_ELEMENTS, i++) 
+    double atlag = 0; // double a pontosabb eredmenyert es 0-rol indulunk
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        atlag += b[i] // Hiba_8: hianyzo pontosvesszo
+        atlag += b[i];
     }
+
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    // Hiba_9: a lefoglalt memoria nincs felszabaditva (delete[] b)
+
+    delete[] b; // Memoria felszabaditva
     return 0;
 }
