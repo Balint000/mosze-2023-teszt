0a0d437580df5ab064fb8f319476fb05443c90db
Balint000 <erdeibalint01@gmail.com>
2026-02-21 11:08:16 +0100

    hibak kommentezve

--- main.cpp
+++ main.cpp
@@ -4,23 +4,29 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    // Hiba_1: NELEMENTS nincs definialva (N_ELEMENTS helyes)
+    int *b = new int[NELEMENTS]; 
+    // Hiba_2: szimpla idezojel; hianyzo pontosvesszo
+    std::cout << '1-100 ertekek duplazasa' 
+    // Hiba_3: hianyos for ciklus
+    for (int i = 0;) 
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    // Hiba_4: hibas feltetel: a feltetel (i) miatt 0-nal azonnal kilep
+    for (int i = 0; i; i++) 
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // Hiba_5: hianyzo pontosvesszo
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // Hiba_6: nincs inicializalva (memoria-szemet lesz benne)
+    // Hiba_7: vesszo hasznalata pontosvesszo helyett
+    for (int i = 0; i < N_ELEMENTS, i++) 
     {
-        atlag += b[i]
+        atlag += b[i] // Hiba_8: hianyzo pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    // Hiba_9: a lefoglalt memoria nincs felszabaditva (delete[] b)
     return 0;
 }
